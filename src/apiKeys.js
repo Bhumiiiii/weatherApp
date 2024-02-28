2c3ee504073c55ea08b79857a97e81aead685b4b
Bhumi Gupta <bhumigupta175@gmail.com>
2024-02-28 22:21:58 +0530

    init

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "a4e59d2a0e14b4cbf5c74a84d86d7014",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
